4cc14cef8616176c37881e457378cdb3a6da1ee3
naymc <naumenkosergeyns@gmail.com>
2019-11-18 11:41:18 +0300

    lesson3

--- src/lesson2/TaskA1.java
+++ src/lesson2/TaskA1.java
@@ -1,4 +1,4 @@
-package sergey.lesson2;
+package lesson2;
 
 /*
 Выведите на экран в цикле 10 раз фразу
@@ -25,20 +25,5 @@ package sergey.lesson2;
 
  */
 public class TaskA1 {
-
-    public static void main(String[] args) {
-        int i = 1;
-        while (true){
-//        for (int i = 1; i <= 10; i++) {
-            if ((i == 3) || (i == 9)) {
-                System.out.println("Меня интересует высокая зарплата!");
-            } else
-                System.out.println("Я изучаю программирование.");
-            i++;
-            if (i>10)
-                break;
-        }
-    }
-
 
 }
